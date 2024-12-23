1414cc39929f81c2f6208c8c3e406eec8c456e49
yanis <benikenyanis1@gmail.com>
2024-12-23 14:16:18 +0100

    Pre

--- src/Form/Form.jsx
+++ src/Form/Form.jsx
@@ -1,6 +1,11 @@
-import {useState} from "react";
+import {useState,useRef} from "react";
 
 export default function Form(){
+    const InputNameRef = useRef()
+    const InputAgeRef = useRef()
+    const InputCityRef = useRef()
+    const InputCountryRef = useRef()
+    const InputtConditionsRef = useRef()
    
     const [InputConditions,setInputConditions] = useState(false)
     const [formValues,setformValues] = useState('')
@@ -18,26 +23,41 @@ export default function Form(){
     }
     return <div className={'container'}>
         <form >
+            {(new Date).toLocaleString()}
 
         <div className="form-group">
                 <label>Name</label>
-                <input type="text" name='name' className='form-control' id="name"  onChange={handelChange}/>
+                <input type="text" name='name' className='form-control' id="name"   ref={InputNameRef}/>
             </div>
 
             <div className="form-group">
                 <label>Age</label>
-                <input type="text" name='city' className='form-control' id="age"  onChange={handelChange}/>
+                <input type="text" name='city' className='form-control' id="age"  ref={InputAgeRef}/>
             </div>
             <div className="form-check">
                
                 <label htmlFor="">jaccepte</label>
-                <input type="checkbox" id="accept"  className='form-check'  onChange={InputConditionChange} />
+                <input type="checkbox" id="accept"  className='form-check'  ref={InputtConditionsRef} />
+            </div>
+            <div className="form-group">
+                <label>City</label>
+                <input type="text" name='city' className='form-control' ref={InputCityRef}/>
             </div>
 
             <div className="form-group">
                
             <button className="btn btn-primary">Save</button>
                 
+            </div>
+            <div className="form-group">
+                <label>Country</label>
+                <label htmlFor="country"></label>
+                <select className="form-control" name="country" ref={InputCountryRef}>
+                    <option value='MA'>Maroc</option>
+                    <option value='DZ'>Algérie</option>
+                    <option value='TN'>Tunisie</option>
+                    <option value='OTHER'>other</option>
+                </select>
             </div>
         </form>
     </div>
