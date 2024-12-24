92a494e8fcd8d6c36676a476bbc9b59f997fada3
yanis <benikenyanis1@gmail.com>
2024-12-24 23:12:50 +0100

    Pre

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Nom from './Component/Nom.jsx';
 import Form from './Form/Form.jsx';
 import Page from './languageSwitcher/Page.jsx';
+import FormValidation from './FormValidation/FormValidation.jsx';
 
 function App() {
   return <>
-    <Page/>
+    <FormValidation/>
   </>
     
 }
